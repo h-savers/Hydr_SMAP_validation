618978022debbffcc40097d40128e9160b4ca346
h-savers <nazzareno.pierdicca@uniroma1.it>
2026-07-07 00:08:14 +0200

    Correct error in readSMOS with longiture instead of longitude

--- src/ReadSMOS.m
+++ src/ReadSMOS.m
@@ -1,4 +1,4 @@
-function SMOS = ReadSMOS(dayOKwithSMOS, SMOSfileOK_SD, SMOSfileOK_SA, SMOSfolderOK, pixelSMOS, lineSMOS)
+function SMOS = ReadSMOS(dayOKwithSMOS, SMOSfileOK_SD, SMOcolocationSMASfileOK_SA, SMOSfolderOK, pixelSMOS, lineSMOS)
 SecInDay=24*60*60 ; 
 for ii=dayOKwithSMOS'
 
@@ -43,7 +43,7 @@ SMOS(ii,1).retrieval_qual_flag_PM_REF=nan(pixelSMOS, lineSMOS) ;
 SMOS(ii,1).latitude_AM=nan(pixelSMOS, lineSMOS) ;
 SMOS(ii,1).latitude_PM=nan(pixelSMOS, lineSMOS) ;
 SMOS(ii,1).longitude_AM=nan(pixelSMOS, lineSMOS) ;
-SMOS(ii,1).longiture_PM=nan(pixelSMOS, lineSMOS) ;
+SMOS(ii,1).longitude_PM=nan(pixelSMOS, lineSMOS) ;
 
 
 end
@@ -87,7 +87,7 @@ SMOS(ii,2).retrieval_qual_flag_PM_REF=nan(pixelSMOS, lineSMOS) ;
 SMOS(ii,2).latitude_AM=nan(pixelSMOS, lineSMOS) ;
 SMOS(ii,2).latitude_PM=nan(pixelSMOS, lineSMOS) ;
 SMOS(ii,2).longitude_AM=nan(pixelSMOS, lineSMOS) ;
-SMOS(ii,2).longiture_PM=nan(pixelSMOS, lineSMOS) ;
+SMOS(ii,2).longitude_PM=nan(pixelSMOS, lineSMOS) ;
 end 
 
 if ismissing(SMOSfolderOK(ii,3)) ==0
